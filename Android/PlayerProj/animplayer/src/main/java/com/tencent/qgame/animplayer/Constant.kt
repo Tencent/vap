c42070313b1c0280eab20ad4cf2ef9ad942994d4
hexleo <hexleo@tencent.com>
2020-09-04 14:24:33 +0800

    feat: 修改错误文案

--- Android/PlayerProj/animplayer/src/main/java/com/tencent/qgame/animplayer/Constant.kt
+++ Android/PlayerProj/animplayer/src/main/java/com/tencent/qgame/animplayer/Constant.kt
@@ -40,10 +40,10 @@ object Constant {
     const val REPORT_ERROR_TYPE_CONFIG_PLUGIN_MIX = 10006 // vapx动画资源获取失败
     const val REPORT_ERROR_TYPE_FILE_ERROR = 10007 // 文件无法读取
 
-    const val ERROR_MSG_EXTRACTOR_EXC = "0x1" // MediaExtractor exception
-    const val ERROR_MSG_DECODE_EXC = "0x2" // MediaCodec exception
-    const val ERROR_MSG_CREATE_THREAD = "0x3" // 线程创建失败
-    const val ERROR_MSG_CREATE_RENDER = "0x4" // render创建失败
+    const val ERROR_MSG_EXTRACTOR_EXC = "0x1 MediaExtractor exception" // MediaExtractor exception
+    const val ERROR_MSG_DECODE_EXC = "0x2 MediaCodec exception" // MediaCodec exception
+    const val ERROR_MSG_CREATE_THREAD = "0x3 thread create fail" // 线程创建失败
+    const val ERROR_MSG_CREATE_RENDER = "0x4 render create fail" // render创建失败
     const val ERROR_MSG_PARSE_CONFIG = "0x5 parse config fail" // 配置解析失败
     const val ERROR_MSG_CONFIG_PLUGIN_MIX = "0x6 vapx fail" // vapx动画资源获取失败
     const val ERROR_MSG_FILE_ERROR = "0x7 file can't read" // 文件无法读取
