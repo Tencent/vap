7fb663d3d8f3abd4b328ed0ae9d4ae475f6eb3f0
irisfdcui <irisfdcui@tencent.com>
2021-06-15 16:06:25 +0800

    1. 修复部分手机不支持yuv420sp的兼容性问题 2. 修复播放宽度不能被4整除的视频时crash的问题 3. 更换demo测试视频

--- Android/PlayerProj/animplayer/src/main/java/com/tencent/qgame/animplayer/YUVShader.kt
+++ Android/PlayerProj/animplayer/src/main/java/com/tencent/qgame/animplayer/YUVShader.kt
@@ -31,28 +31,30 @@ object YUVShader {
 
     const val FRAGMENT_SHADER = "precision mediump float;\n" +
             "uniform sampler2D sampler_y;\n" +
-            "uniform sampler2D sampler_uv;\n" +
+            "uniform sampler2D sampler_u;\n" +
+            "uniform sampler2D sampler_v;\n" +
             "varying vec2 v_TexCoordinateAlpha;\n" +
             "varying vec2 v_TexCoordinateRgb;\n" +
+            "uniform mat3 convertMatrix;\n" +
+            "uniform vec3 offset;\n" +
             "\n" +
             "void main() {\n" +
-            "   float y1,u1,v1;\n" +
-            "   float y2,u2,v2;\n" +
-            "   y2 = texture2D(sampler_y,v_TexCoordinateAlpha).r;\n" +
-            "   u2 = texture2D(sampler_uv,v_TexCoordinateAlpha).r- 0.5;\n" +
-            "   v2 = texture2D(sampler_uv,v_TexCoordinateAlpha).a - 0.5;\n" +
-            "   y1 = texture2D(sampler_y,v_TexCoordinateRgb).r;\n" +
-            "   u1 = texture2D(sampler_uv,v_TexCoordinateRgb).r- 0.5;\n" +
-            "   v1 = texture2D(sampler_uv,v_TexCoordinateRgb).a - 0.5;\n" +
-            "   vec3 rgb1;\n" +
-            "   vec3 rgb2;\n" +
-            "   rgb1.r = y1 + 1.403 * v1;\n" +
-            "   rgb1.g = y1 - 0.344 * u1 - 0.714 * v1;\n" +
-            "   rgb1.b = y1 + 1.770 * u1;\n" +
-            "   rgb2.r = y2 + 1.403 * v2;\n" +
-            "   rgb2.g = y2 - 0.344 * u2 - 0.714 * v2;\n" +
-            "   rgb2.b = y2 + 1.770 * u2;\n" +
-            "   gl_FragColor=vec4(rgb1, rgb2.r);\n" +
+            "   highp vec3 yuvColorAlpha;\n" +
+            "   highp vec3 yuvColorRGB;\n" +
+            "   highp vec3 rgbColorAlpha;\n" +
+            "   highp vec3 rgbColorRGB;\n" +
+            "   yuvColorAlpha.x = texture2D(sampler_y,v_TexCoordinateAlpha).r;\n" +
+            "   yuvColorRGB.x = texture2D(sampler_y,v_TexCoordinateRgb).r;\n" +
+            "   yuvColorAlpha.y = texture2D(sampler_u,v_TexCoordinateAlpha).r;\n" +
+            "   yuvColorAlpha.z = texture2D(sampler_v,v_TexCoordinateAlpha).r;\n" +
+            "   yuvColorRGB.y = texture2D(sampler_u,v_TexCoordinateRgb).r;\n" +
+            "   yuvColorRGB.z = texture2D(sampler_v,v_TexCoordinateRgb).r;\n" +
+            "   yuvColorAlpha += offset;\n" +
+            "   yuvColorRGB += offset;\n" +
+            "   rgbColorAlpha = convertMatrix * yuvColorAlpha; \n" +
+            "   rgbColorRGB = convertMatrix * yuvColorRGB; \n" +
+            "   gl_FragColor=vec4(rgbColorRGB, rgbColorAlpha.r);\n" +
             "}"
+
     // RGB2*Alpha+RGB1*(1-Alpha)
 }
