f00f59f38ab47fe2a850f9e3a87416263ccffb6f
bellebinwu <bellebinwu@tencent.com>
2025-07-08 14:20:32 +0800

    change license

--- iOS/QGVAPlayer/QGVAPlayer/Classes/Controllers/QGAnimatedImageDecodeThreadPool.h
+++ iOS/QGVAPlayer/QGVAPlayer/Classes/Controllers/QGAnimatedImageDecodeThreadPool.h
@@ -1,7 +1,7 @@
 // QGAnimatedImageDecodeThreadPool.h
 // Tencent is pleased to support the open source community by making vap available.
 //
-// Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+// Copyright (C) 2020 Tencent.  All rights reserved.
 //
 // Licensed under the MIT License (the "License"); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
